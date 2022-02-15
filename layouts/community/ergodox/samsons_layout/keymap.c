9280bb7e89581ec2ccf7df67d0c57b472f5c47ad
Andriy Samsonyuk <a@samsonyuk.de>
2022-02-15 12:57:13 +0100

    using numpad numbers instead of numbers in the game layer

--- layouts/community/ergodox/samsons_layout/keymap.c
+++ layouts/community/ergodox/samsons_layout/keymap.c
@@ -230,7 +230,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
 [GAME] = LAYOUT_ergodox(
-       KC_ESC,   KC_1,         KC_2,         KC_3,     KC_4,    KC_5,    KC_TRNS,
+       KC_ESC,   KC_KP_1,      KC_KP_2,      KC_KP_3,  KC_KP_4, KC_KP_5, KC_NUBS,
        KC_TAB,   KC_Q,         KC_W,         KC_E,     KC_R,    KC_T,    KC_TRNS,
        KC_P,     KC_A,         KC_S,         KC_D,     KC_F,    KC_G,
        KC_LSFT,  KC_Z,         KC_X,         KC_C,     KC_V,    KC_B,    MO(MOUSE),
@@ -238,11 +238,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                            KC_DEL,  KC_INS, 
                                                     KC_PGUP,
                                   KC_ENT,  KC_BSPC, KC_PGDN,
-                                                                 KC_TRNS,    KC_6, KC_7, KC_8,    KC_9,    KC_0,    MO(COMMAND),
-                                                                 KC_TRNS,    KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_NLCK,
-                                                                             KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_SLCK,
-                                                                 MO(MOUSE),  KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                                                                    KC_VOLU,    KC_VOLD, KC_RALT, KC_RCTL, KC_TRNS,
+                                                                 KC_KP_DOT,  KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0,    MO(COMMAND),
+                                                                 KC_TRNS,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_NLCK,
+                                                                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_SLCK,
+                                                                 MO(MOUSE),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,
+                                                                                    KC_VOLU,   KC_VOLD, KC_RALT, KC_RCTL,    KC_TRNS,
                                                                  KC_LEFT, KC_RGHT,
                                                                  KC_UP,
                                                                  KC_DOWN, KC_BSPC, KC_SPC
